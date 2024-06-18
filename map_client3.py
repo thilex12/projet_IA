d623869ffb4c22e9de26ad807fbae5208e4faea0
thilex12 <thilex12@gmail.com>
2024-06-18 16:34:09 +0200

    carte client 3

--- map_client3.py
+++ map_client3.py
@@ -9,7 +9,7 @@ def read_json(path):
 
 def csv_to_json(csv_path, json_path):
     df = pd.read_csv(csv_path)
-    return df.to_json(json_path)
+    return write_json(df, json_path)
 
 # csv_to_json('Data_Arbre.csv', 'data.json')
 
@@ -17,13 +17,18 @@ def csv_to_json(csv_path, json_path):
 df_json = read_json('data.json')
 
 def carte_to_htlm(json):
-    map = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
+    map = folium.Map(zoom_start=12, location=[49.8476780339, 3.2866348474000002])
     
     for i in range(len(json)):
         folium.Circle(
-            location = [json.iloc[i]['longitude'], json.iloc[i]['latitude']],
-            radius =  (json.iloc[i]['tronc_diam']/3.1415) * 0.2 + 1,
-            popup=json.iloc[i]['fk_nomtech'],
+            location = [json.iloc[i]['latitude'], json.iloc[i]['longitude']],
+            radius =  (json.iloc[i]['tronc_diam']/3.1415) * 0.1 + 1,
+            popup=f'<div style="width : 200px">Position de l\'arbre : {json.iloc[i]['latitude'], json.iloc[i]['longitude']}<br>'
+                  f'Remarquable : {json.iloc[i]['remarquable']}<br>'
+                  f'Stade de développement : {json.iloc[i]['fk_stadedev']}<br>'
+                  f'Etat de l\'arbre : {json.iloc[i]['fk_arb_etat']}<br>'
+                  f'Quartier : {json.iloc[i]['clc_quartier']}<br>'
+                  f'Secteur : {json.iloc[i]['clc_secteur']}</div>'
             ).add_to(map)
     
     return map.save('map.html')    
